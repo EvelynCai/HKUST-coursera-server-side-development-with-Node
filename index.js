8501ff1c3c8edff96402d54c1bea8c277ca683a4
Evelyn Cai <2018ycai@gmail.com>
2021-05-24 20:07:47 -0400

    hw1

---
+++ index.js
@@ -0,0 +1,25 @@
+const morgan = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+const server = http.createServer(app);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
